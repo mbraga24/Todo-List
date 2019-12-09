136e9bb7ba98ada315ff8f62d2727d3bf63df85a
Marlon Braga <rdg.brg@gmail.com>
2019-12-09 14:49:19 -0500

    Updates

---
+++ precache-manifest.31f2628dab03a3b1d67a100dc38e4a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bfdac51933643d60d8b3baeea856ff",
+    "url": "/To-do-List/index.html"
+  },
+  {
+    "revision": "6942a0ada2a10a1de0f4",
+    "url": "/To-do-List/static/css/main.22edb290.chunk.css"
+  },
+  {
+    "revision": "492bc7b19e622a107cf7",
+    "url": "/To-do-List/static/js/2.31f7a188.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/To-do-List/static/js/2.31f7a188.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6942a0ada2a10a1de0f4",
+    "url": "/To-do-List/static/js/main.31b905ee.chunk.js"
+  },
+  {
+    "revision": "b930df946d11b8e8b902",
+    "url": "/To-do-List/static/js/runtime-main.a3951203.js"
+  }
+]);
